bc3c66ac2cc09130a490b4366942512faf60193b
tech-a47 <tech-a47@Tech-A47s-MacBook-Air.local>
2019-02-19 11:05:34 +0000

    content for capybara intro

--- week10/capybara/capybara_intro/capybara_intro.rb
+++ week10/capybara/capybara_intro/capybara_intro.rb
@@ -3,3 +3,14 @@ require 'capybara'
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
+
+
+session = Capybara::Session.new(:chrome)
+session2 = Capybara::Session.new(:chrome)
+
+session.visit('http://www.toolsqa.com/automation-practice-form/')
+session2.visit('https://www.google.com/')
+session.fill_in 'firstname', with: 'test'
+session.choose 'exp-1'
+session.choose 'sex-0'
+sleep 2
