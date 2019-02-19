7cbcb12cbc51d5faa11e75c9c32b775516728327
tech-a47 <tech-a47@Tech-A47s-MacBook-Air.local>
2019-02-19 14:18:15 +0000

    setting up file structure for bbc pom

---
+++ week10/capybara/bbc_pom/lib/bbcsite.rb
@@ -0,0 +1,15 @@
+require_relative 'pages/bbc_homepage'
+require_relative 'pages/bbc_sign_in_page'
+
+
+
+class BBCSite
+
+  def bbc_homepage
+
+  end
+
+  def bbc_sign_in_page
+
+  end
+end
---
+++ week10/capybara/bbc_pom/spec/bbc_login_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'BBC Login Functionality' do
+
+
+  context 'It should allow the user to login with valid details' do
+    
+  end
+
+  context 'It should respond with the correct error when incorrect details are input' do
+
+  end
+
+end
