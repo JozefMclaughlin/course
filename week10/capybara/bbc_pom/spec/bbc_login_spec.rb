79058700070b16ae41751ad2099bdf35dd167c4f
tech-a47 <tech-a47@Tech-A47s-MacBook-Air.local>
2019-02-19 14:35:41 +0000

    current_url bug fix

--- week10/capybara/bbc_pom/spec/bbc_login_spec.rb
+++ week10/capybara/bbc_pom/spec/bbc_login_spec.rb
@@ -4,10 +4,17 @@ describe 'BBC Login Functionality' do
 
 
   context 'It should allow the user to login with valid details' do
-    @bbcsite = BBCSite.new
-    @bbcsite.bbc_homepage.visit_homepage
-    expect(@bbcsite.current_url).to eq "https://www.bbc.co.uk/"
-    @bbcsite.bbc_homepage.click_sign_in
+
+    it "should form a happy path" do
+      @bbcsite = BBCSite.new
+      @bbcsite.bbc_homepage.visit_homepage
+      expect(@bbcsite.bbc_homepage.current_url).to eq "https://www.bbc.co.uk/"
+      @bbcsite.bbc_homepage.click_sign_in
+
+
+      
+    end
+
   end
 
   context 'It should respond with the correct error when incorrect details are input' do
