2b7fb96cda88847530b1418b3b87b432fba614cf
tech-a47 <tech-a47@Tech-A47s-MacBook-Air.local>
2019-02-19 14:07:52 +0000

    setting up bbc pom

---
+++ week10/capybara/bbc_pom/spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require 'capybara'
+
+Rspec.configure do |config|
+  config.formatter = :documentation
+end
+
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.configure do |config|
+  config.default_driver = :chrome   #Sets chrome as the default browser
+  config.default_max_weihjt_time = 5 #Wait time for the asynchronous processes to finish
+  config.ignore_hidden_elements = true #can only see visable page ignore_hidden_elements
+  config.default_selector = :css #CSS selector is used as default
+end  
